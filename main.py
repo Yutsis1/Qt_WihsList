3f5e06c7749694cc9db688e0407ee24084f3fc93
Roman Yutsis <yucis@author.spb.ru>
2019-10-14 14:51:03 +0300

    make first links between DB and UI

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sys
 import mysql.connector
 from mysql.connector import Error
-from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QAction, QLineEdit, QMessageBox, QComboBox, QLabel
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QMainWindow, QAction, QLineEdit, QMessageBox, QComboBox, QLabel
 from PyQt5.QtCore import QCoreApplication
 
-class Example(QWidget):
+from DB_halper import DB_halper
 
-    def __init__(self):
+class MainWindow(QMainWindow):
+
+    def __init__(self, dbHalper):
         super().__init__()
         self.title = 'WishListQt Test'
         self.left = 300
@@ -14,6 +16,8 @@ class Example(QWidget):
         self.width = 1000
         self.height = 300
         self.initUI()
+        self.dbhalper = dbHalper
+
 
 
     def initUI(self):
@@ -21,12 +25,12 @@ class Example(QWidget):
 
         # buttons
         addBtn = QPushButton('Add new wish', self)
-        addBtn.clicked.connect(QCoreApplication.instance().quit)
+        addBtn.clicked.connect(self.addToWishes)
         addBtn.resize(addBtn.sizeHint())
         addBtn.move(50, 50)
 
         showBtn = QPushButton('Show wishes', self)
-        showBtn.clicked.connect(QCoreApplication.instance().quit)
+        showBtn.clicked.connect(self.showWishes)
         showBtn.resize(showBtn.sizeHint())
         showBtn.move(50, 100)
 
@@ -38,34 +42,76 @@ class Example(QWidget):
 
 
         # editText fields
-        nameEditText = QLineEdit('name of wish', self)
-        nameEditText.resize(nameEditText.sizeHint())
-        nameEditText.move(150, 50)
+        self.nameEditText = QLineEdit('name of wish', self)
+        self.nameEditText.resize(self.nameEditText.sizeHint())
+        self.nameEditText.move(150, 50)
 
-        priceEditText = QLineEdit('price', self)
-        priceEditText.resize(priceEditText.sizeHint())
-        priceEditText.move(300, 50)
+        self.priceEditText = QLineEdit('price', self)
+        self.priceEditText.resize(self.priceEditText.sizeHint())
+        self.priceEditText.move(300, 50)
 
-        commentEditText = QLineEdit('comment', self)
-        commentEditText.resize(commentEditText.sizeHint())
-        commentEditText.move(450, 50)
+        self.commentEditText = QLineEdit('comment', self)
+        self.commentEditText.resize(self.commentEditText.sizeHint())
+        self.commentEditText.move(450, 50)
 
-        linkEditText = QLineEdit('link', self)
-        linkEditText.resize(linkEditText.sizeHint())
-        linkEditText.move(600, 50)
+        self.linkEditText = QLineEdit('link', self)
+        self.linkEditText.resize(self.linkEditText.sizeHint())
+        self.linkEditText.move(600, 50)
 
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.setWindowTitle(self.title)
         self.show()
+
+    def addToWishes(self):
+        name = self.nameEditText.text()
+        comment = self.commentEditText.text()
+        link = self.linkEditText.text()
+        try:
+            price = int(self.priceEditText.text())
+            self.dbhalper.insert_new_to_WishList(name, price, comment, link)
+        except ValueError:
+            print("write number")
+
+
+    def showWishes(self):
+        self.wishScreen = WishWindow()
+        self.wishScreen.show()
     #
     # def initDB(self):
     #
     #
     # def addToDB(self):
 
+class WishWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'WishList'
+        self.left = 300
+        self.top = 300
+        self.width = 1000
+        self.height = 300
+
+
+
+# custom exceptbook for hooking Qt exceprions (problem found in PyCharm)
+def my_exception_hook(exctype, value, traceback):
+    # Print the error and traceback
+    print(exctype, value, traceback)
+    # Call the normal Exception hook after
+    sys._excepthook(exctype, value, traceback)
+    sys.exit(1)
+
 
 if __name__ == '__main__':
+    # Back up the reference to the exceptionhook
+    sys._excepthook = sys.excepthook
+    sys.excepthook = my_exception_hook
 
+
+    dbhalper =  DB_halper()
     app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
+    ex = MainWindow(dbhalper)
+    try:
+        sys.exit(app.exec_())
+    except:
+        print("Exiting")
